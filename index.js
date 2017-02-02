f7ca9019115f6094e6af839ca739d7ab3fb9c202
Guillaume Mantopoulos <guillaume.mantopoulos@gmail.com>
2017-02-02 10:43:59 +0100

    Fix wrong comments

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const csv = require('csvtojson')
+const csv = require('csvtojson');
 
 // Initialize Server
 const app = new express();
 app.set('port', process.env.PORT || 3000);
-
-// Only serve static assets in production, in development, webpack handles those.
 app.use(express.static('public'));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// Start Server
+// Root Path
 app.get('/', function(req, res) {
 	parseCSV()
 		.then(newsletters => {
@@ -26,10 +24,10 @@ app.get('/', function(req, res) {
 		});
 });
 
+// Start Server
 app.listen(app.get('port'), (req, res) => {
 	console.log(`Server running on port ${app.get('port')}`);
 })
-
 
 function parseCSV() {
 	return new Promise((resolve,reject) => {
